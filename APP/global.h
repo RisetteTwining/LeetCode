21b8fe8e54a690539bfee39bdc1b05789075ec0a
risettetwinings <frierenbeyondjourneysend@hotmail.com>
2024-05-02 10:44:35 +0800

    Add global.h and main.cpp

---
+++ APP/global.h
@@ -0,0 +1,41 @@
+﻿/*-----------------------------------------------------------------------------------------
+**
+** Copyright: Juliana, 2024~
+**
+** Filename: global.h
+**
+** Description: This File defines some global functions
+** 
+** Design Annotation:
+**
+** Author:
+**		Juliana, start writing this file in May 2024
+** 
+** Modification History:
+**		May 2nd 2024, create this file
+**		$Revision$	
+**		$Date$
+**		$Author$
+**		$Log$
+** 
+**-----------------------------------------------------------------------------------------
+*/
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+/*-----------------------------------------------------------------------------------------
+**										  Include 
+**-----------------------------------------------------------------------------------------
+*/
+#include <map>
+#include <queue>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+
+#endif // _COMMON_H_
---
+++ APP/main.cpp
@@ -0,0 +1,58 @@
+﻿/*-----------------------------------------------------------------------------------------
+**
+** Copyright: Juliana, 2024~
+**
+** Filename: main.cpp
+**
+** Description: This File defines the entry of the application
+** 
+** Including Function:
+**		main
+**
+** Design Annotation:
+**
+** Author:
+**		Juliana, start writing this file in May 2024
+**
+** Modification History:
+**		May 2nd 2024, create this file
+**		$Revision$
+**		$Date$
+**		$Author$
+**		$Log$
+**
+**-----------------------------------------------------------------------------------------
+*/
+/*-----------------------------------------------------------------------------------------
+**										  Include
+**-----------------------------------------------------------------------------------------
+*/
+
+/*-----------------------------------------------------------------------------------------
+**									Function Definition
+**-----------------------------------------------------------------------------------------
+*/
+/*.BH--------------------------------------------------------------------------------------
+**
+** Function Name: main
+** 
+** Description: This function defines the entry of the application
+** 
+** Input Parameter:
+**
+** Output Parameter:
+**
+** Return Value:
+**
+** Design Annotation:
+** 
+** Modification History:
+**		May 2nd 2024, create this function
+** 
+**.EH--------------------------------------------------------------------------------------
+*/
+int main(void)
+{
+	return 0;
+}
+/* END of main */
